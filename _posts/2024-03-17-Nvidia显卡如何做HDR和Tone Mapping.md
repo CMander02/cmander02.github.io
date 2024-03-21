13cda9faece2a97eb3e54ab5ef60ac4d747382d4
CMander02 <15946840899@163.com>
2024-03-20 21:59:24 -0400

    20240320，一键生成标准开头的markdow的程序

---
+++ _posts/2024-03-17-Nvidia显卡如何做HDR和Tone Mapping.md
@@ -0,0 +1,27 @@
+---
+layout: post
+title:  "Nvidia显卡如何做HDR和Tone Mapping"
+date:   2023-03-17 17:49:30 -0500
+categories: jekyll update
+---
+[toc]
+
+超分辨是通过软硬件增加画面的分辨率和码率的一种手段。本文只介绍**Windows操作系统**基于**Nvidia显卡+potplayer播放器**的超分辨方式。
+
+### 0.打开HDR
+- Windows 设置-系统-屏幕-打开“使用HDR”
+- 设置-应用-视频播放-打开“自动处理视频以增强其效果”
+- Windows商店下载校色软件，手动校色，保存校色文件。
+
+### 1.Nvidia控制面板开启对应功能
+打开Nvidia控制面板，左侧选视频-调整视频图像设置--RTX视频增强-超分辨率，按需勾选质量。
+
+### 2.PotPlayer设置
+安装PotPlayer后，选项-视频，
+- 视频渲改为“内置 Direct3D 11 视频渲染器”
+- 尺寸调整选择“自动选择（推荐）”
+- 勾选下方的“D3D11 GPU 超分辨率”
+保存配置后重启，播放任意视频时，只要视频比起原本的尺寸有放大，就会自动超分辨率。
+
+### 3.其他未设置
+- [tone mapping](https://docs.google.com/document/d/1OIVKk8njrDTELsIZUrTBod_LdPB1sz9FieK6h1DfzF0/edit)
---
+++ createpost.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import datetime
+
+# 确保用户提供了一个命令行参数作为标题
+if len(sys.argv) < 2:
+    print("错误: 请提供一个标题。")
+    print("用法: python createpost.py 我是标题")
+    sys.exit(1)
+
+# 从命令行参数获取标题
+title = sys.argv[1]
+
+# 定义_posts文件夹的路径
+posts_folder = "./_posts"
+
+# 如果_posts文件夹不存在，则创建它
+if not os.path.exists(posts_folder):
+    os.makedirs(posts_folder)
+
+# 获取当前日期和时间
+current_date = datetime.datetime.now()
+
+# 创建文件名格式为 yyyy-mm-dd-title.markdown
+filename = f"{current_date.strftime('%Y-%m-%d')}-{title}.markdown"
+
+# 定义完整的文件路径
+full_path = os.path.join(posts_folder, filename)
+
+# 定义要写入文件的内容
+content = f"""---
+layout: post
+title:  "{title}"
+date:  {current_date.strftime('%Y-%m-%d %H:%M:%S')} -0500
+categories: jekyll update
+---
+[TOC]
+***
+"""
+
+# 写入内容到Markdown文件
+with open(full_path, "w", encoding="utf-8") as file:
+    file.write(content)
+
+print(f"文件已成功创建于: {full_path}")
